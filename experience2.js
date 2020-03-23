700bde17da7bf3c472933672899fb005b892d8b2
George Wyscaver <gwyscaver3@gmail.com>
2020-03-23 14:17:14 -0700

    updated experience2.js

--- experience2.js
+++ experience2.js
@@ -1,43 +1,53 @@
-//Created javascript event listener to detect form submission when "Let's Explor" button has been clicked
-document.getElementById("submitDestination").addEventListener("click", function (){
-  
-  document.getElementById("experienceContent").innerHTML="";
-    //console.log("submitted")
-    const location = document.getElementById("locationName").value;
-    //console.log(location)
-    grabExperiences(location)
-    //JS animate and scroll top
-    const elmnt = document.getElementById("portfolio");
-    elmnt.scrollIntoView();
+//Local storage 
+window.onload = function(e){
+  document.getElementById('locationName').value = localStorage.getItem('userInput');
+}
+const valid = true;
 
+//Created javascript event listener to detect form submission when 'Find KWHEST' button has been clicked
+document.getElementById('submitDestination').addEventListener('click', function (){
+  document.getElementById('experienceContent').innerHTML='';
+  //console.log('submitted')
+  const location = document.getElementById('locationName').value;
+  //console.log(location)
+  if (location.length <=50 && /^([a-zA-Z]\-*\s*[a-zA-Z]+)+[,]\s*[a-zA-Z]{2,}$/.test(location)) {
+    localStorage.setItem('userInput', location);
+    // console.log('input is valid' + /^([a-zA-Z]\-*\s*[a-zA-Z]+)+[,]\s*[a-zA-Z]{2,}$/.test(location))
+    document.getElementById('text-danger').innerText = '';
+    document.getElementById('experienceBox').classList.remove('invisible');
+
+    grabExperiences(location);
+      //JS animate and scroll top
+    const elmnt = document.getElementById('portfolio')
+    elmnt.scrollIntoView();
+  } else {
+    document.getElementById('text-danger').innerText = 'Your input was invalid';
+    // console.log('input is not valid' + location)
+    valid = false;
+  }
 });
 
-// grabs information from "Yelp"
+// grabs information from 'Yelp'
 function grabExperiences(location){
     // link to yelp api
     const yelpURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location='+location+'&categories=active,arts&radius=16094&rating=3.5&price=2,3&limit=5';
     
-    // const headers = new Headers()
-    // headers.append("Authorization", "Bearer " + "5g5JBhiRRQYPP_XdUOB0yplJ7sMeepIg7suBSMb8ZgpEAmwGuGAO3--CzOc-3w0Zx35sw19opkOG02GSGDexwEMf0h5USlpO1ivs3niI9aL4A-UfrOzFg0BET6N6XXYx")
-    // const request = new Request(yelpURL,{
-    //   method:"GET", headers:headers
-    // })
 
-    fetch(yelpURL, {
+    $.ajax({
+      url:yelpURL,
       method:'GET',
-      headers:{
-        'X-Requested-With': 'XMLHttpRequest',
-        'Origin':'https://cors-anywhere.herokuapp.com',
-        'Authorization':'Bearer ' + '5g5JBhiRRQYPP_XdUOB0yplJ7sMeepIg7suBSMb8ZgpEAmwGuGAO3--CzOc-3w0Zx35sw19opkOG02GSGDexwEMf0h5USlpO1ivs3niI9aL4A-UfrOzFg0BET6N6XXYx'
-      },
-      'crossDomain': true, 
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ('Authorization', 'Bearer ' + 'W_SgCu6EU7XV8PZuKPE-7-1hIQEsbo-xkeLImSvhBGrfR7Pyii4gPTP2jYI24hGLwC3pOr1sxPZ4yAjXbOu8zoH_Swhuq0TbRSVnFo4qbdsX3Cz8EvIMDW4Bx6B2XnYx');
+    },
+
+
     }).then(function(response) { 
-        const container = document.createElement("div");
+        const container = document.createElement('div');
         
         response.businesses.forEach(function(val){ 
           const link = document.createElement('a');
           link.setAttribute('target', '_blank');
-          link.setAttribute('href', + val.url);
+          link.setAttribute('href', val.url);
 
           const h3 = document.createElement('h3');
           const h3Text = document.createTextNode(val.name);
@@ -46,17 +56,17 @@ function grabExperiences(location){
           container.appendChild(link);
 
           const address = document.createElement('h4');
-          const addressText = document.createTextNode('address:' + val.location.address1);
+          const addressText = document.createTextNode('Address:' + " " + val.location.address1);
           address.appendChild(addressText);
           container.appendChild(address);
 
           const price = document.createElement('h4');
-          const priceText = document.createTextNode('price:' + val.price);
+          const priceText = document.createTextNode('Price:' + " " + val.price);
           price.appendChild(priceText);
           container.appendChild(price);
 
           const rating = document.createElement('h4');
-          const ratingText = document.createTextNode('rating:' + val.rating);
+          const ratingText = document.createTextNode('Rating:' + " " + val.rating);
           rating.appendChild(ratingText);
           container.appendChild(rating);
 
@@ -69,8 +79,21 @@ function grabExperiences(location){
 
         })
         document.getElementById('experienceContent').appendChild(container);
-        document.getElementById('portfolioModal5').appendChild(container);
-        
         
       });
     };
+
+  let color = 255;
+  let countDownInterval = setInterval(function() {
+    if (color > 0) {
+      if (color == 125) {
+        document.querySelector('#trip-overview').style.color='#fff';
+        document.querySelector('#expSubHead').classList.remove('text-muted');
+        document.querySelector('#expSubHead').style.color='#fff';
+      }
+        document.querySelector('#particles-js').style.backgroundColor=`rgb(${color}, ${color}, ${color})`
+        color--;
+        } else {
+            clearInterval(countDownInterval);
+    }
+}, 500);
